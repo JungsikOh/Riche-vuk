e1a021029bd96d0715b59f9f04b9d4f71ff2e8ed
nemostar <nemostar51@gmail.com>
2025-02-10 20:53:35 +0900

    feat : Raytracing 초기화함수 추가

---
+++ Riche/Rendering/IRenderPass.cpp
@@ -0,0 +1,149 @@
+#include "IRenderPass.h"
+
+IRenderPass::IRenderPass(VkDevice device, VkPhysicalDevice physicalDevice) {
+  vkGetBufferDeviceAddressKHR =
+      reinterpret_cast<PFN_vkGetBufferDeviceAddressKHR>(vkGetDeviceProcAddr(device, "vkGetBufferDeviceAddressKHR"));
+  vkCreateAccelerationStructureKHR =
+      reinterpret_cast<PFN_vkCreateAccelerationStructureKHR>(vkGetDeviceProcAddr(device, "vkCreateAccelerationStructureKHR"));
+  vkDestroyAccelerationStructureKHR =
+      reinterpret_cast<PFN_vkDestroyAccelerationStructureKHR>(vkGetDeviceProcAddr(device, "vkDestroyAccelerationStructureKHR"));
+  vkGetAccelerationStructureBuildSizesKHR = reinterpret_cast<PFN_vkGetAccelerationStructureBuildSizesKHR>(
+      vkGetDeviceProcAddr(device, "vkGetAccelerationStructureBuildSizesKHR"));
+  vkGetAccelerationStructureDeviceAddressKHR = reinterpret_cast<PFN_vkGetAccelerationStructureDeviceAddressKHR>(
+      vkGetDeviceProcAddr(device, "vkGetAccelerationStructureDeviceAddressKHR"));
+  vkCmdBuildAccelerationStructuresKHR =
+      reinterpret_cast<PFN_vkCmdBuildAccelerationStructuresKHR>(vkGetDeviceProcAddr(device, "vkCmdBuildAccelerationStructuresKHR"));
+  vkBuildAccelerationStructuresKHR =
+      reinterpret_cast<PFN_vkBuildAccelerationStructuresKHR>(vkGetDeviceProcAddr(device, "vkBuildAccelerationStructuresKHR"));
+  vkCmdTraceRaysKHR = reinterpret_cast<PFN_vkCmdTraceRaysKHR>(vkGetDeviceProcAddr(device, "vkCmdTraceRaysKHR"));
+  vkGetRayTracingShaderGroupHandlesKHR =
+      reinterpret_cast<PFN_vkGetRayTracingShaderGroupHandlesKHR>(vkGetDeviceProcAddr(device, "vkGetRayTracingShaderGroupHandlesKHR"));
+  vkCreateRayTracingPipelinesKHR =
+      reinterpret_cast<PFN_vkCreateRayTracingPipelinesKHR>(vkGetDeviceProcAddr(device, "vkCreateRayTracingPipelinesKHR"));
+
+  // 함수 로딩 확인
+  if (!vkGetBufferDeviceAddressKHR || !vkCreateAccelerationStructureKHR || !vkDestroyAccelerationStructureKHR ||
+      !vkGetAccelerationStructureBuildSizesKHR || !vkGetAccelerationStructureDeviceAddressKHR ||
+      !vkCmdBuildAccelerationStructuresKHR || !vkBuildAccelerationStructuresKHR || !vkCmdTraceRaysKHR ||
+      !vkGetRayTracingShaderGroupHandlesKHR || !vkCreateRayTracingPipelinesKHR) {
+    throw std::runtime_error("Failed to load Vulkan Ray Tracing function pointers!");
+  }
+
+  rayTracingPipelineProperties.sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_RAY_TRACING_PIPELINE_PROPERTIES_KHR;
+  VkPhysicalDeviceProperties2 deviceProperties2{};
+  deviceProperties2.sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_PROPERTIES_2;
+  deviceProperties2.pNext = &rayTracingPipelineProperties;
+  vkGetPhysicalDeviceProperties2(physicalDevice, &deviceProperties2);
+
+  accelerationStructureFeatures.sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_ACCELERATION_STRUCTURE_FEATURES_KHR;
+  VkPhysicalDeviceFeatures2 deviceFeatures2{};
+  deviceFeatures2.sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_FEATURES_2;
+  deviceFeatures2.pNext = &accelerationStructureFeatures;
+  vkGetPhysicalDeviceFeatures2(physicalDevice, &deviceFeatures2);
+}
+
+VkDeviceAddress IRenderPass::GetVkDeviceAddress(VkDevice device, VkBuffer buffer) {
+  VkBufferDeviceAddressInfoKHR bufferDeviceAddressInfo = {};
+  bufferDeviceAddressInfo.sType = VK_STRUCTURE_TYPE_BUFFER_DEVICE_ADDRESS_INFO_KHR;
+  bufferDeviceAddressInfo.buffer = buffer;
+
+  VkDeviceAddress address = vkGetBufferDeviceAddress(device, &bufferDeviceAddressInfo);
+  return address;
+}
+
+void IRenderPass::CreateAccelerationStructure(VkDevice device, VkPhysicalDevice physicalDevice,
+                                              AccelerationStructure& accelerationStructure, VkAccelerationStructureTypeKHR type,
+                                              VkAccelerationStructureBuildSizesInfoKHR buildSizeInfo) {
+  // Buffer and memory
+  VkBufferCreateInfo bufferCreateInfo{};
+  bufferCreateInfo.sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO;
+  bufferCreateInfo.size = buildSizeInfo.accelerationStructureSize;
+  bufferCreateInfo.usage = VK_BUFFER_USAGE_ACCELERATION_STRUCTURE_STORAGE_BIT_KHR | VK_BUFFER_USAGE_SHADER_DEVICE_ADDRESS_BIT;
+  VK_CHECK(vkCreateBuffer(device, &bufferCreateInfo, nullptr, &accelerationStructure.buffer));
+  VkMemoryRequirements memoryRequirements{};
+  vkGetBufferMemoryRequirements(device, accelerationStructure.buffer, &memoryRequirements);
+  VkMemoryAllocateFlagsInfo memoryAllocateFlagsInfo{};
+  memoryAllocateFlagsInfo.sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_FLAGS_INFO;
+  memoryAllocateFlagsInfo.flags = VK_MEMORY_ALLOCATE_DEVICE_ADDRESS_BIT_KHR;
+  VkMemoryAllocateInfo memoryAllocateInfo{};
+  memoryAllocateInfo.sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO;
+  memoryAllocateInfo.pNext = &memoryAllocateFlagsInfo;
+  memoryAllocateInfo.allocationSize = memoryRequirements.size;
+  memoryAllocateInfo.memoryTypeIndex =
+      VkUtils::FindMemoryTypeIndex(physicalDevice, memoryRequirements.memoryTypeBits, VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT);
+  VK_CHECK(vkAllocateMemory(device, &memoryAllocateInfo, nullptr, &accelerationStructure.memory));
+  VK_CHECK(vkBindBufferMemory(device, accelerationStructure.buffer, accelerationStructure.memory, 0));
+  // Acceleration structure
+  VkAccelerationStructureCreateInfoKHR accelerationStructureCreate_info{};
+  accelerationStructureCreate_info.sType = VK_STRUCTURE_TYPE_ACCELERATION_STRUCTURE_CREATE_INFO_KHR;
+  accelerationStructureCreate_info.buffer = accelerationStructure.buffer;
+  accelerationStructureCreate_info.size = buildSizeInfo.accelerationStructureSize;
+  accelerationStructureCreate_info.type = type;
+  vkCreateAccelerationStructureKHR(device, &accelerationStructureCreate_info, nullptr, &accelerationStructure.handle);
+  // AS device address
+  VkAccelerationStructureDeviceAddressInfoKHR accelerationDeviceAddressInfo{};
+  accelerationDeviceAddressInfo.sType = VK_STRUCTURE_TYPE_ACCELERATION_STRUCTURE_DEVICE_ADDRESS_INFO_KHR;
+  accelerationDeviceAddressInfo.accelerationStructure = accelerationStructure.handle;
+  accelerationStructure.deviceAddress = vkGetAccelerationStructureDeviceAddressKHR(device, &accelerationDeviceAddressInfo);
+}
+
+void IRenderPass::CreateAccelerationStructureBuffer(VkDevice device, VkPhysicalDevice physicalDevice,
+                                                    AccelerationStructure& accelerationStructure,
+                                                    VkAccelerationStructureBuildSizesInfoKHR buildSizeInfo) {
+  VkBufferCreateInfo bufferCreateInfo{};
+  bufferCreateInfo.sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO;
+  bufferCreateInfo.size = buildSizeInfo.accelerationStructureSize;
+  bufferCreateInfo.usage = VK_BUFFER_USAGE_ACCELERATION_STRUCTURE_STORAGE_BIT_KHR | VK_BUFFER_USAGE_SHADER_DEVICE_ADDRESS_BIT;
+  VK_CHECK(vkCreateBuffer(device, &bufferCreateInfo, nullptr, &accelerationStructure.buffer));
+
+  VkMemoryRequirements memoryRequirements{};
+  vkGetBufferMemoryRequirements(device, accelerationStructure.buffer, &memoryRequirements);
+
+  VkMemoryAllocateFlagsInfo memoryAllocateFlagsInfo{};
+  memoryAllocateFlagsInfo.sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_FLAGS_INFO;
+  memoryAllocateFlagsInfo.flags = VK_MEMORY_ALLOCATE_DEVICE_ADDRESS_BIT_KHR;
+
+  VkMemoryAllocateInfo memoryAllocateInfo{};
+  memoryAllocateInfo.sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO;
+  memoryAllocateInfo.pNext = &memoryAllocateFlagsInfo;
+  memoryAllocateInfo.allocationSize = memoryRequirements.size;
+  memoryAllocateInfo.memoryTypeIndex =
+      VkUtils::FindMemoryTypeIndex(physicalDevice, memoryRequirements.memoryTypeBits, VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT);
+
+  VK_CHECK(vkAllocateMemory(device, &memoryAllocateInfo, nullptr, &accelerationStructure.memory));
+  VK_CHECK(vkBindBufferMemory(device, accelerationStructure.buffer, accelerationStructure.memory, 0));
+}
+
+ScratchBuffer IRenderPass::CreateScratchBuffer(VkDevice device, VkPhysicalDevice physicalDevice, VkDeviceSize size) {
+  ScratchBuffer scratchBuffer{};
+
+  // Buffer and memory
+  VkBufferCreateInfo bufferCreateInfo{};
+  bufferCreateInfo.sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO;
+  bufferCreateInfo.size = size;
+  bufferCreateInfo.usage = VK_BUFFER_USAGE_STORAGE_BUFFER_BIT | VK_BUFFER_USAGE_SHADER_DEVICE_ADDRESS_BIT;
+  VK_CHECK(vkCreateBuffer(device, &bufferCreateInfo, nullptr, &scratchBuffer.handle));
+
+  VkMemoryRequirements memoryRequirements{};
+  vkGetBufferMemoryRequirements(device, scratchBuffer.handle, &memoryRequirements);
+
+  VkMemoryAllocateFlagsInfo memoryAllocateFlagsInfo{};
+  memoryAllocateFlagsInfo.sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_FLAGS_INFO;
+  memoryAllocateFlagsInfo.flags = VK_MEMORY_ALLOCATE_DEVICE_ADDRESS_BIT_KHR;
+  VkMemoryAllocateInfo memoryAllocateInfo = {};
+  memoryAllocateInfo.sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO;
+  memoryAllocateInfo.pNext = &memoryAllocateFlagsInfo;
+  memoryAllocateInfo.allocationSize = memoryRequirements.size;
+  memoryAllocateInfo.memoryTypeIndex =
+      VkUtils::FindMemoryTypeIndex(physicalDevice, memoryRequirements.memoryTypeBits, VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT);
+  VK_CHECK(vkAllocateMemory(device, &memoryAllocateInfo, nullptr, &scratchBuffer.memory));
+  VK_CHECK(vkBindBufferMemory(device, scratchBuffer.handle, scratchBuffer.memory, 0));
+
+  // Buffer device address
+  VkBufferDeviceAddressInfoKHR bufferDeviceAddresInfo{};
+  bufferDeviceAddresInfo.sType = VK_STRUCTURE_TYPE_BUFFER_DEVICE_ADDRESS_INFO;
+  bufferDeviceAddresInfo.buffer = scratchBuffer.handle;
+  scratchBuffer.deviceAddress = vkGetBufferDeviceAddressKHR(device, &bufferDeviceAddresInfo);
+
+  return scratchBuffer;
+}
--- Riche/Rendering/IRenderPass.h
+++ Riche/Rendering/IRenderPass.h
@@ -1,18 +1,62 @@
 #pragma once
 
+#include "VkUtils/ResourceManager.h"
+
+// Holds information for a ray tracing scratch buffer that is used as a temporary storage
+struct ScratchBuffer {
+  uint64_t deviceAddress = 0;
+  VkBuffer handle = VK_NULL_HANDLE;
+  VkDeviceMemory memory = VK_NULL_HANDLE;
+};
+
+// Ray tracing acceleration structure
+struct AccelerationStructure {
+  VkAccelerationStructureKHR handle;
+  uint64_t deviceAddress = 0;
+  VkDeviceMemory memory;
+  VkBuffer buffer;
+};
+
 class Camera;
 class Editor;
 
 class IRenderPass {
  public:
+  PFN_vkGetBufferDeviceAddressKHR vkGetBufferDeviceAddressKHR;
+  PFN_vkCreateAccelerationStructureKHR vkCreateAccelerationStructureKHR;
+  PFN_vkDestroyAccelerationStructureKHR vkDestroyAccelerationStructureKHR;
+  PFN_vkGetAccelerationStructureBuildSizesKHR vkGetAccelerationStructureBuildSizesKHR;
+  PFN_vkGetAccelerationStructureDeviceAddressKHR vkGetAccelerationStructureDeviceAddressKHR;
+  PFN_vkCmdBuildAccelerationStructuresKHR vkCmdBuildAccelerationStructuresKHR;
+  PFN_vkBuildAccelerationStructuresKHR vkBuildAccelerationStructuresKHR;
+  PFN_vkCmdTraceRaysKHR vkCmdTraceRaysKHR;
+  PFN_vkGetRayTracingShaderGroupHandlesKHR vkGetRayTracingShaderGroupHandlesKHR;
+  PFN_vkCreateRayTracingPipelinesKHR vkCreateRayTracingPipelinesKHR;
+
+  VkPhysicalDeviceRayTracingPipelinePropertiesKHR rayTracingPipelineProperties{};
+  VkPhysicalDeviceAccelerationStructureFeaturesKHR accelerationStructureFeatures{};
+
+  IRenderPass() = default;
+  IRenderPass(VkDevice device, VkPhysicalDevice physicalDevice);
+  ~IRenderPass() = default;
+
   virtual void Initialize(VkDevice device, VkPhysicalDevice physicalDevice, VkQueue queue, VkCommandPool commandPool, Camera* camera,
-                          Editor* editor,
-                          const uint32_t width, const uint32_t height) = 0;
+                          Editor* editor, const uint32_t width, const uint32_t height) = 0;
   virtual void Cleanup() = 0;
 
   virtual void Update() = 0;
 
   virtual void Draw(VkSemaphore renderAvailable) = 0;
+
+  VkDeviceAddress GetVkDeviceAddress(VkDevice device, VkBuffer buffer);
+
+  void CreateAccelerationStructure(VkDevice device, VkPhysicalDevice physicalDevice, AccelerationStructure& accelerationStructure,
+                                   VkAccelerationStructureTypeKHR type, VkAccelerationStructureBuildSizesInfoKHR buildSizeInfo);
+  void CreateAccelerationStructureBuffer(VkDevice device, VkPhysicalDevice physicalDevice,
+                                         AccelerationStructure& accelerationStructure,
+                                         VkAccelerationStructureBuildSizesInfoKHR buildSizeInfo);
+
+  ScratchBuffer CreateScratchBuffer(VkDevice device, VkPhysicalDevice physicalDevice, VkDeviceSize size);
 
  private:
   // - Rendering Pipeline
